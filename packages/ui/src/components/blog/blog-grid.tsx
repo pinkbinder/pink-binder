c01d1d0511a7820de97a2787b8247a9b4f3d2872
NiftyAndy <andy@niftyleague.com>
2026-07-16 20:27:26 -0400

    fix: prettier-format packages/ui (blog-grid, blog-inline-text)

--- packages/ui/src/components/blog/blog-grid.tsx
+++ packages/ui/src/components/blog/blog-grid.tsx
@@ -88,13 +88,7 @@ function isFilterValueActive(
 }
 
 type FilterGroupKey =
-  | 'type'
-  | 'generation'
-  | 'list'
-  | 'illustrator'
-  | 'expansion'
-  | 'pokemon'
-  | 'themes'
+  'type' | 'generation' | 'list' | 'illustrator' | 'expansion' | 'pokemon' | 'themes'
 
 interface GroupedFilters {
   type: string | null
